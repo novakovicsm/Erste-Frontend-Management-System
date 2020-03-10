6be2ac0c26fbc755eb8f67e33316adf49afae9f9
novakovicsm <novakovicsm@users.noreply.github.com>
2020-03-10 12:59:22 +0100

    Update cloud.js

--- loadtests/cloud.js
+++ loadtests/cloud.js
@@ -1,9 +1,9 @@
 import {check, group, sleep} from "k6";
 import http from "k6/http";
 export let options = {stages: [
-		{duration: "10s",target: 15},
-		{duration: "20s", target: 15},
-		{duration: "10s",target: 0}],
+		{duration: "10s",target: 30},
+		{duration: "20s", target: 30},
+		{duration: "10s",target: 5}],
 	thresholds: {"http_req_duration": ["p(95)<250"]},
 	ext: {loadimpact: {
 			projectID: 3483149,
